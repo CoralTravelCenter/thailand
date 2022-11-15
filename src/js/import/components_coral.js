7a7af57fb4806260b239c0a29d791e2494ea1a03
Alexander Shin <alexander.shin@coral.ru>
2022-11-15 16:54:58 +0300

    coral upd

--- src/js/import/components_coral.js
+++ src/js/import/components_coral.js
@@ -1,33 +1,45 @@
 // import "%components%/scrollfix/scrollfix.js";
-// import "%components%/preloader/preloader.js";
-// import "%components%/animator/animator.js";
+// import "%components%/tab/tab.js";
 import "%components%/tab/tab.js";
-
-
 document.addEventListener('DOMContentLoaded', function(){
 
-    $(document).on('click', '.dm-btn--js', function(e){
-        e.preventDefault();
+    // $(document).on('click', '.dm-btn.dm-btn--benefits4', function(e){
+    //     e.preventDefault();
+    //     var $this = $(this);
+    //     var $href = $this.attr('href');
+    //     var $target = $( '#hotels-set' );
+    //     var $trigger;
+    //     if( $href == "#turkie" ){
+    //         $trigger = 'Турция';
+    //     }
+    //     else if( $href == "#uae" ){
+    //         $trigger = 'ОАЭ';
+    //     }
+    //     else if( $href == "#egypt" ){
+    //         $trigger = 'Египет';
+    //     }
+    //     else if( $href == "#maldives" ){
+    //         $trigger = 'Мальдивы';
+    //     }
+    //     else if( $href == "#shrilanka" ){
+    //         $trigger = 'Шри-Ланка';
+    //     }
+    //     else if( $href == "#russia" ){
+    //         $trigger = 'Россия';
+    //     }
+    //     if( $target.length ){
+    //         $('html, body').stop().animate( {
+    //             'scrollTop': $target.offset().top - 120
+    //             }, 900, 'swing', function () {
+    //             $( '[data-group="' +  $trigger + '"]' ).trigger('click');
+    //         });
+    //     }
+    // });
+
+    $('.dm-tab__title').on('click', function(){
         var $this = $(this);
-        var $href = $this.attr('href');
-        var $target = $( '#hotels-set' );
-        var $trigger;
-        if( $href == "#Rayong" ){
-            $trigger = 'Районг (Rayong)';
-        }
-        else if( $href == "#Chang" ){
-            $trigger = 'о. Ко Чанг (Koh Chang)';
-        }
-        else if( $href == "#pattayya" ){
-            $trigger = '*';
-        }
-        if( $target.length ){
-            $('html, body').stop().animate( {
-                'scrollTop': $target.offset().top - 120
-                }, 900, 'swing', function () {
-                $( '[data-group="' +  $trigger + '"]' ).trigger('click');
-            });
-        }
+        var $name = $this.data('show');
+        $('[data-content-for="' + $name +  '"] [data-group="*" ]').trigger('click');
     });
 
 });
